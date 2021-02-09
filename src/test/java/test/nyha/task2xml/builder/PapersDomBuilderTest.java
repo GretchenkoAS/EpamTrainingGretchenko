12f225cbf449bf1471df86f3b4da2608d02c9f2d
Andrey Gretchenko <gret.andrey@gmail.com>
2021-02-09 11:38:23 +0300

    fix tests

--- src/test/java/test/nyha/task2xml/builder/PapersDomBuilderTest.java
+++ src/test/java/test/nyha/task2xml/builder/PapersDomBuilderTest.java
@@ -1,11 +1,15 @@
 package test.nyha.task2xml.builder;
 
 import com.nyha.task2xml.builder.AbstractPapersBuilder;
-import com.nyha.task2xml.entity.Paper;
+import com.nyha.task2xml.builder.PapersDomBuilder;
+import com.nyha.task2xml.entity.*;
 import com.nyha.task2xml.exception.ParserException;
 import com.nyha.task2xml.factory.PaperBuilderFactory;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import java.time.LocalDate;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -13,13 +17,56 @@ import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertNotEquals;
 
 public class PapersDomBuilderTest {
+
+    Set<Paper> expected;
+
+    @BeforeTest
+    public void setUp() {
+        expected = new HashSet<>();
+        ArtPaper paper1 = new ArtPaper();
+        paper1.setId("a1");
+        paper1.setLanguage("eng");
+        paper1.setTitle("Shrek");
+        paper1.setType(PaperType.MAGAZINE);
+        paper1.setReleaseDate(LocalDate.parse("2015-01-01"));
+        paper1.setMonthly(true);
+        Chars chars1 = new Chars();
+        chars1.setColored(true);
+        chars1.setPages(14);
+        chars1.setGlossy(true);
+        chars1.setSubscribeIndex(83472);
+        paper1.setChars(chars1);
+        paper1.setAssigment(Assigment.FOR_KIDS);
+        expected.add(paper1);
+
+        SciencePaper paper2 = new SciencePaper();
+        paper2.setId("s1");
+        paper2.setLanguage("rus");
+        paper2.setTitle("Квант");
+        paper2.setType(PaperType.MAGAZINE);
+        paper2.setReleaseDate(LocalDate.parse("1970-02-23"));
+        paper2.setMonthly(true);
+        Chars chars2 = new Chars();
+        chars2.setColored(true);
+        chars2.setPages(24);
+        chars2.setGlossy(true);
+        chars2.setSubscribeIndex(34738);
+        paper2.setChars(chars2);
+        paper2.setAttestationOrganization("ВАК");
+        expected.add(paper2);
+    }
+
+    @AfterTest
+    public void tearDown() {
+        expected = null;
+    }
+
     @Test
     public void buildPlanesPositiveTest() throws ParserException {
         String type = "dom";
         AbstractPapersBuilder builder = PaperBuilderFactory.createPaperBuilder(type);
-        builder.buildSetPapers("src/test/resources/periodicalTest.xml");
+        builder.buildSetPapers("src/test/resources/periodical/periodicalTest.xml");
         Set<Paper> actual = builder.getPapers();
-        Set<Paper> expected = PaperData.getPapers();
         assertEquals(actual, expected);
     }
 
@@ -27,7 +74,7 @@ public class PapersDomBuilderTest {
     public void buildPlanesNegativeTest() throws ParserException {
         String type = "dom";
         AbstractPapersBuilder builder = PaperBuilderFactory.createPaperBuilder(type);
-        builder.buildSetPapers("src/test/resources/periodicalTest.xml");
+        builder.buildSetPapers("src/test/resources/periodical/periodicalTest.xml");
         Set<Paper> actual = builder.getPapers();
         Set<Paper> expected = new HashSet<>();
         assertNotEquals(actual, expected);
